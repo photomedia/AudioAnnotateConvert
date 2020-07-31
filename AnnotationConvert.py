b7dacdc63e5060fbf1a40e59b8fad35ec68112a3
Tomasz Neugebauer <photomedia@gmail.com>
2020-07-31 15:37:54 -0400

    Added CORS so that external applications like Swallow can call on the Flask conversion service as an API Added support for REVERSE conversion, from Avalon XML back to Swallow Text in both command line script and Flask app Command line script takes an optional “-r” parameter to specify the reverse conversion (from Avalon XML to Swallow Text) Added samples for the REVERSE conversion, sample1Reversed.txt, sample2Reversed.txt, sample3Reversed.txt and sample4Reversed.txt, in all cases, these were converted back to Swallow Text (from the generated sample1output.xml, sample2output.xml, sample3output.xml and sample4output.xml using the script.

--- AnnotationConvert.py
+++ AnnotationConvert.py
@@ -3,6 +3,19 @@ import sys
 import string
 import getopt
 import re
+import xml.etree.ElementTree as ET
+
+def processOneItemXML(Item, f2):
+    ItemLabel = Item.get('label')
+    line_to_write = ItemLabel+'\n\n'
+    f2.write(line_to_write)
+    for Span in Item.findall('Span'):
+        label = Span.get('label')
+        speaker = label.split('.', 1)[0]
+        annotation = label.split('.', 1)[1]
+        begin = Span.get('begin')
+        line_to_write = speaker+'\n'+begin+'\n'+annotation.lstrip()+'\n\n'
+        f2.write(line_to_write)
 
 def processOneItem(ItemToProcess, f2):
     #process one Item:
@@ -37,8 +50,9 @@ def processOneItem(ItemToProcess, f2):
 def main(argv):
     inputfile = ''
     outputfile = ''
+    reverseConvert = False
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+        opts, args = getopt.getopt(argv, "rhi:o:", ["ifile=", "ofile="])
     except getopt.GetoptError:
         sys.exit(2)
     for opt, arg in opts:
@@ -49,29 +63,46 @@ def main(argv):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
+        if opt in ("-r"):
+            reverseConvert = True
+    if (reverseConvert == True):
+        #Convert from XML to Text
+        with open(inputfile) as f1:
+            with open(outputfile, 'w') as f2:
+                tree = ET.parse(f1)
+                root = tree.getroot()
+                if root.tag == "Items":
+                    for Item in root.findall('Item'):
+                        processOneItemXML(Item, f2)
+                else:
+                    Item = root
+                    processOneItemXML(Item, f2)
 
-    with open(inputfile) as f1:
-        with open(outputfile, 'w') as f2:
-            #set the source item
-            lines = f1.readlines()
-            special_line_indexes = []
-            for i, line in enumerate(lines): 
-                if ('END\n' == line):
-                    #extract line index for lines that contain END
-                    special_line_indexes.append(i + 1)
-            length = len(special_line_indexes) 
-            #check if multiple files included
-            if length>1:
-                startIndex = 0
-                f2.write("<Items>")
-                for i in range(length): 
-                    endIndex = special_line_indexes[i]+2
-                    #print (str(startIndex)+' '+str(endIndex))
-                    ItemToProcess=lines[startIndex:endIndex]
-                    processOneItem(ItemToProcess, f2)
-                    startIndex=endIndex+1
-                f2.write("</Items>")
-            else:
-                processOneItem(lines, f2)
+
+    else:
+        #Convert from Text to XML
+        with open(inputfile) as f1:
+            with open(outputfile, 'w') as f2:
+                #set the source item
+                lines = f1.readlines()
+                special_line_indexes = []
+                for i, line in enumerate(lines): 
+                    if ('END\n' == line):
+                        #extract line index for lines that contain END
+                        special_line_indexes.append(i + 1)
+                length = len(special_line_indexes) 
+                #check if multiple files included
+                if length>1:
+                    startIndex = 0
+                    f2.write("<Items>")
+                    for i in range(length): 
+                        endIndex = special_line_indexes[i]+2
+                        #print (str(startIndex)+' '+str(endIndex))
+                        ItemToProcess=lines[startIndex:endIndex]
+                        processOneItem(ItemToProcess, f2)
+                        startIndex=endIndex+1
+                    f2.write("</Items>")
+                else:
+                    processOneItem(lines, f2)
 if __name__ == "__main__":
     main(sys.argv[1:])
--- index.py
+++ index.py
@@ -1,6 +1,23 @@
 from flask import Flask, request
+from flask_cors import CORS
+import xml.etree.ElementTree as ET
+
 import re
 app = Flask(__name__)
+cors = CORS(app)
+
+def processOneItemXML(Item):
+    ItemLabel = Item.get('label')
+    line_to_write = ItemLabel+'\n\n'
+    convertedAnnotationText=line_to_write
+    for Span in Item.findall('Span'):
+        label = Span.get('label')
+        speaker = label.split('.', 1)[0]
+        annotation = label.split('.', 1)[1]
+        begin = Span.get('begin')
+        line_to_write = speaker+'\n'+begin+'\n'+annotation.lstrip()+'\n\n'
+        convertedAnnotationText=convertedAnnotationText+line_to_write
+    return(convertedAnnotationText)
 
 def processOneItem(ItemToProcess):
     #process one Item:
@@ -64,6 +81,24 @@ function fetchConversion() {
         }
     })
 }
+
+function fetchConversionToText() {
+    var myform = document.getElementById(\"myform\");
+    var fd = new FormData(myform );
+    var annotation = document.getElementById(\"ConvertedAvalonXML\").value;
+    $.ajax({
+        url: "/convertToText",
+        data: annotation,
+        cache: false,
+        processData: false,
+        contentType: 'xml',
+        dataType: 'text',
+        type: 'POST',
+        success: function (result) {
+            $('#annotation').val(result);
+        }
+    })
+}
 </script>
 
     </head>
@@ -74,13 +109,16 @@ function fetchConversion() {
     <label for=\"annotation\">Paste SW Format Annotation:</label>
     <textarea class=\"form-control\" id=\"annotation\" name=\"annotation\" rows=\"12\"></textarea>
     <button id=\"btnSubmit\" type=\"submit\" class=\"btn btn-primary\"> <i class='far fa-arrow-alt-circle-down'></i> Convert to Avalon XML <i class='far fa-arrow-alt-circle-down'></i></button>
+    <button id=\"copyButtonText\" type="button" style="float:right" class=\"btn btn-default\"><i class="fa fa-clone" aria-hidden="true"></i> select & copy Text</button>
      </div>
-     </form>
-     <button id=\"copyButton\" class=\"btn btn-default\"><i class="fa fa-clone" aria-hidden="true"></i> select & copy</button>
+
+     <button id=\"copyButton\" type="button" class=\"btn btn-default\"><i class="fa fa-clone" aria-hidden="true"></i> select & copy XML</button>
+     <button id=\"btnSubmitToText\" type=\"submit\" class=\"btn btn-primary\" style="float:right"> <i class='far fa-arrow-alt-circle-up'></i> Convert to Text <i class='far fa-arrow-alt-circle-up'></i></button>
      <textarea id=\"ConvertedAvalonXML\" class=\"form-control\" rows=\"10\">
     """;
     output=output +"""
     </textarea>
+    </form>
     <br />
     Source Code: <a href=\"https://github.com/photomedia/AudioAnnotateConvert\">https://github.com/photomedia/AudioAnnotateConvert</a>
     <script>
@@ -89,8 +127,19 @@ function fetchConversion() {
         fetchConversion();
     });
 
+
+    $( \"#btnSubmitToText\" ).click(function( event ) {
+        event.preventDefault();
+        fetchConversionToText();
+    });
+
    $(\"#copyButton\").click(function(){
     $(\"#ConvertedAvalonXML\").select();
+    document.execCommand('copy');
+   });
+
+   $(\"#copyButtonText\").click(function(){
+    $(\"#annotation\").select();
     document.execCommand('copy');
    });
 
@@ -127,6 +176,24 @@ def api_convert():
             convertedAnnotationText=convertedAnnotationText+"</Items>"
         else:
             convertedAnnotationText=processOneItem(lines)
+
+        return (convertedAnnotationText)
+
+@app.route('/convertToText', methods = ['POST'])
+def api_convertToText():
+    if request.method == 'POST':
+        #return ("Received: " + request.headers['Content-Type'] + request.get_data(True, True, False) )
+        annotationText = request.get_data(True, True, False);
+        convertedAnnotationText = "";
+
+        root = ET.fromstring(annotationText)
+
+        if root.tag == "Items":
+            for Item in root.findall('Item'):
+                convertedAnnotationText=convertedAnnotationText+processOneItemXML(Item)
+        else:
+            Item = root
+            convertedAnnotationText=processOneItemXML(Item)
 
         return (convertedAnnotationText)
 
